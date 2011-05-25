a9bc9e0f0daac56d3beb27d6e5c01fe5492dd120
Ralph Morelli <ram8647@gmail.com>
2011-05-24 23:59:03 -0400

    Modified and tested DbTest.

--- src/haiti/server/testfile/DbTest.java
+++ src/haiti/server/testfile/DbTest.java
@@ -1,6 +1,7 @@
 package haiti.server.testfile;
 
 
+import java.awt.FileDialog;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,49 +11,66 @@ import java.sql.Statement;
 
 public class DbTest
 {
-  public static void main(String[] args) throws ClassNotFoundException
-  {
-    // load the sqlite-JDBC driver using the current class loader
-    Class.forName("org.sqlite.JDBC");
-    
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-      
-      statement.executeUpdate("drop table if exists beneficiary");
-      statement.executeUpdate("create table beneficiary (sms string)");
-      statement.executeUpdate("insert into  beneficiary values('f=tom&l=jones&a=11 Main Street&c=Hartford&z=06106')");
-      statement.executeUpdate("insert into beneficiary values('f=sarah&l=smith&a=12 Main Street&c=Hartford&z=06106')");
-      ResultSet rs = statement.executeQuery("select * from beneficiary");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("sms = " + rs.getString("sms"));
-        
-      }
-    }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory", 
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
-    }
-  }
+	public static final String DB_MESSAGE_TABLE = "message_log";
+	public static final String DB_MESSAGE_ID = "id";
+	public static final String DB_MESSAGE_TIME = "timestamp";
+	public static final String DB_MESSAGE_COLUMN = "message_text";
+	public static final String DB_MESSAGE_STATUS = "processed";
+	
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		// load the sqlite-JDBC driver using the current class loader
+		Class.forName("org.sqlite.JDBC");
+
+		Connection connection = null;
+		try
+		{
+			String path = "jdbc:sqlite:" + System.getProperty("user.dir").toString() + "/db/haiti.db";
+			System.out.println("PATH=" + path);
+			
+			
+			// create a database connection
+			connection = DriverManager.getConnection(path);
+			//connection = DriverManager.getConnection("jdbc:sqlite:db/haiti.db");
+			connection = DriverManager.getConnection("jdbc:sqlite:/Users/rmorelli/workspace_hg_posit/haiti-server-initial/db/haiti.db");
+			//connection = DriverManager.getConnection("jdbc:sqlite:/Users/rmorelli/workspace_hg_posit/haiti-server-initial/db/haiti.db");
+
+			Statement statement = connection.createStatement();
+			System.out.println(statement.toString());
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+			
+			ResultSet rs = statement.executeQuery("select * from message_log");
+			rs.next();
+			while(!rs.isAfterLast()) {
+				// Write the result set
+				//System.out.println("msg = " + rs.getString("message_text"));
+				String msg = rs.getString(DB_MESSAGE_ID) + "|" 
+					+ rs.getString(DB_MESSAGE_STATUS) + "|"
+					+ rs.getString(DB_MESSAGE_TIME) + "|" 
+					+ rs.getString(DB_MESSAGE_COLUMN);
+				System.out.println(msg);
+				rs.next();
+			}
+			System.out.println("Done");
+		}
+		catch(SQLException e)
+		{
+			// if the error message is "out of memory", 
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				if(connection != null)
+					connection.close();
+			}
+			catch(SQLException e)
+			{
+				// connection close failed.
+				System.err.println(e);
+			}
+		}
+	}
 }
